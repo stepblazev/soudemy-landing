9868415aa523063550b787ed362e4543acee584b
SBViside <ohvatuki@gmail.com>
2023-08-28 11:44:08 +0300

    changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import { Tooltip } from 'bootstrap';
+
 // BOOTSTRAP TOOLTIP ENABLING
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl));
