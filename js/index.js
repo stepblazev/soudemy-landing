40940894342b8be09f5d5de9644e0d22d341df55
Eugene Ohvatuk <77917435+stepblazev@users.noreply.github.com>
2023-08-28 10:50:36 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,3 @@
+// BOOTSTRAP TOOLTIP ENABLING
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
